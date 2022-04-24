9ff43c64534e9d0d87d608fd0e3056a641469b55
Muhammad Hasyim Asy'ari <hasy1m@MacBook-Air-Muhammad.local>
2022-04-24 23:09:56 +0700

    4th Practicum

---
+++ app/src/main/java/com/example/bp_4intentimplisit/AboutActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bp_4intentimplisit
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate( savedInstanceState: Bundle? ) {
+        super.onCreate( savedInstanceState )
+        setContentView( R.layout.activity_about )
+
+        //Instance Image View
+        val imgCall: ImageView      = findViewById( R.id.imageView_Call )
+        val imgLocation: ImageView  = findViewById( R.id.imageView_Location )
+        val imgBrowser: ImageView   = findViewById( R.id.imageView_Browser )
+
+        //Event di Masing-masing Image View
+        //Dial Number
+        imgCall.setOnClickListener {
+            val intentDial: Intent = Uri.parse( "tel: 081222888123" ).let {
+                Intent( Intent.ACTION_DIAL )
+            }
+            startActivity( intentDial )
+        }
+        //Location
+        imgLocation.setOnClickListener {
+            val intentMap: Intent = Intent( Intent.ACTION_VIEW ).apply {
+                data = Uri.parse( "geo: -7.752754, 114.201473" )
+            }
+            startActivity( intentMap )
+        }
+        //Browser
+        imgBrowser.setOnClickListener {
+            val intentBrowser: Intent = Uri.parse( "https://amikom.ac.id/" ).let {
+                Intent( Intent.ACTION_VIEW )
+            }
+            startActivity( intentBrowser )
+        }
+    }
+}
